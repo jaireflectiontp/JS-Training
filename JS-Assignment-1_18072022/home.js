21d1b0f0059b6a1f2e86b93699e82cbe0af47f25
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-07-18 17:54:06 +0530

    js-assignment-1

---
+++ JS-Assignment-1_18072022/home.js
@@ -0,0 +1,22 @@
+var number1 = 25;
+var number2 = 25;
+var number3 = 20;
+var number4 = 15;
+var sum = number1 + number2;
+var sub = number1 - number2;
+var mult = number1 * number2;
+var div = number1 / number2;
+var rem = number1 % number2;
+var calc = ((number1 + number3 - number2) + (number1 * number2)) / ((number2 * number3) - (number4 - number1))
+
+var firstname = 'Jay';
+var lastname = 'Rautel';
+
+
+document.write('addition=', sum, '<br/>')
+document.write('substraction=', sub, '<br/>')
+document.write('multiplication=', mult, '<br/>')
+document.write('division=', div, '<br/>')
+document.write('remainder =', rem, '<br/>')
+document.write('calculation =', calc, '<br/>')
+document.write('FullName :',firstname, lastname)
