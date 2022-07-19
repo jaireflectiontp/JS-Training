a2fb924218342b4b2085622f5560b33d9c04ddfc
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-07-19 10:48:59 +0530

    js-calculator-assignment

--- JS-Assignment-2_18072022/calc.js
+++ JS-Assignment-2_18072022/calc.js
@@ -1,36 +1,36 @@
+
 function addition() {
     var number1 = parseInt(document.getElementById('num1').value);
     var number2 = parseInt(document.getElementById('num2').value);
     var sum = number1 + number2;
-    document.getElementById('result').innerText = sum;
+    document.getElementById('result').innerHTML = sum;
 }
 function substraction() {
     var number1 = parseInt(document.getElementById('num1').value);
     var number2 = parseInt(document.getElementById('num2').value);
     var sub = number2 - number1;
-    document.getElementById('result').innerText = sub;
+    document.getElementById('result').innerHTML = sub;
 }
 function multiplication() {
     var number1 = parseInt(document.getElementById('num1').value);
     var number2 = parseInt(document.getElementById('num2').value);
     var mul = number1 * number2;
-    document.getElementById('result').innerText = mul;
+    document.getElementById('result').innerHTML = mul;
 }
 function division() {
     var number1 = parseInt(document.getElementById('num1').value);
     var number2 = parseInt(document.getElementById('num2').value);
     var div = number2 / number1;
-    document.getElementById('result').innerText = div;
+    document.getElementById('result').innerHTML = div;
 }
 function remainder() {
     var number1 = parseInt(document.getElementById('num1').value);
     var number2 = parseInt(document.getElementById('num2').value);
     var remain = number2 % number1;
-    document.getElementById('result').innerText = remain;
+    document.getElementById('result').innerHTML = remain;
 }
-function clear(){
-    var number1 = (document.getElementById('num1').value);
-    var number2 = (document.getElementById('num2').value);
-    var clear = "-";
-    document.getElementById("result").innertext = clear;
+function reset() {
+   document.getElementById('num1').value="";
+   document.getElementById('num2').value="";
+document.getElementById('result').innerHTML = "";
 }
