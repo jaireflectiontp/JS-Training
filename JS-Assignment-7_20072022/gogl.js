24f9405ac9ea79a5d25be18e7cbed0a178056bf7
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-07-21 23:06:19 +0530

    google form

--- JS-Assignment-7_20072022/gogl.js
+++ JS-Assignment-7_20072022/gogl.js
@@ -19,24 +19,34 @@ function create() {
     }
     else if (firstname == '') {
         document.getElementById('Efirstname').innerHTML = '*please enter your first name*';
+        document.getElementById('firstname').style.border='1px solid red';
     }
     else if (lastname == '') {
         document.getElementById('Elastname').innerHTML = '*Please enter your last name*';
+        document.getElementById('lastname').style.border='1px solid red';
     }
     else if (mail == '') {
         document.getElementById('Email').innerHTML = '*Please enter your e-mail address*';
+        document.getElementById('mail').style.border='1px solid red';
+    }
+    else if (mail !='/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/' ) {
+        document.getElementById('Email').innerHTML = '*Please check your e-mail address*';
     }
     else if (password == '') {
         document.getElementById('Epassword').innerHTML = '*Please enter the password*';
+        document.getElementById('password').style.border='1px solid red';
     }
     else if (password.length < 8) {
-        document.getElementById('Epassword').innerHTML = '*Please enter the password*';
+        document.getElementById('Epassword').innerHTML = '*Password should contain min 8 character*';
+
     }
     else if (cpassword == '') {
         document.getElementById('Ecpassword').innerHTML = '*Please re-enter the password *';
+        document.getElementById('cpassword').style.border='1px solid red';
     }
-    else if (cpassword.length < 8) {
-        document.getElementById('Epassword').innerHTML = '*Please enter the password*';
+    else if (password != cpassword) {
+        document.getElementById('Ecpassword').innerHTML = '*Please enter the password correctly*';
+
     }
     else {
         document.getElementById('success').innerHTML = 'Thank You!'
