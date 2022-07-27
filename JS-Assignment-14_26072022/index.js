7299f51c8fcec14aefa9475909994e7e0ab4bc8a
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-07-27 23:04:31 +0530

    fgf

--- JS-Assignment-14_26072022/index.js
+++ JS-Assignment-14_26072022/index.js
@@ -1,30 +1,30 @@
 //debugger
 var img = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg']
 
-document.getElementById('photos').innerHTML = '<img id="jpg0" img-index="0"  src="' + img[0] + '" class="picsize"/>'
+document.getElementById('photos').innerHTML = '<img id="jpg0" data-image="0"  src="' + img[0] + '" class="picsize"/>'
 
-var dots = '';
-for (var i = 0 ; i < images.length; i++) {
+ var dots = '';
+for (var i = 0 ; i < img.length; i++) {
     dots += '<span onclick=" showslide('+i+')" class=" divslide" > '+i+' </span>'
 }
 document.getElementById('dots').innerHTML=dots;
 
-function showslide(guru){
-    document.getElementById('result').innerHTML = '<img id="image' +(guru)+'"  data-photo="' +(guru)+ '" src="' + images[(guru)] + '" />'
+function showslide(0){
+    document.getElementById('photos').innerHTML = '<img id="img' +(0)+'"  data-image="' +(0)+ '" src="' + img[(0)] + '" />'
 }
 
-function next() {
-    var currentimg = parseInt(document.querySelectorAll('#result img ')[0].dataset.photo);
-    document.getElementById('result').innerHTML = '<img id="image' +(currentimg + 1)+'"  data-photo="' +(currentimg + 1)+ '" src="' + images[(currentimg + 1)] + '" />'
+function ahead() {
+    var activeimg = parseInt(document.querySelectorAll('#photos img ')[0].dataset.image);
+    document.getElementById('photos').innerHTML = '<img id="jpg' +(activeimg + 1)+'"  data-image="' +(activeimg + 1)+ '" src="' + img[(activeimg + 1)] + '"  class="picsize"/>'
 
 }
-function prev() {
-    var currentimg = parseInt(document.querySelectorAll('#result img ')[0].dataset.photo);
-    document.getElementById('result').innerHTML = '<img id="image' +(currentimg - 1)+ '"  data-photo="' +(currentimg - 1)+ '" src="' + images[(currentimg - 1)] + '" />'
+function back() {
+    var activeimg = parseInt(document.querySelectorAll('#photos img ')[0].dataset.image);
+    document.getElementById('photos').innerHTML = '<img id="jpg' +(activeimg - 1)+ '"  data-image="' +(activeimg - 1)+ '" src="' + img[(activeimg - 1)] + '" class="picsize"/>'
 
 }
 
-function forward() {
+/*function forward() {
     var activeimg = (document.querySelectorAll('#photos img')[0].img-index);
     document.getElementById('photos').innerHTML = '<img id="jpg' + parseInt(activeimg + 1) + '" data-index="' + parseInt(activeimg + 1) + '" src="' + img[parseInt(activeimg+1)] + '"  class="picsize"/>'
 
@@ -33,4 +33,4 @@ function forward() {
 function backward() {
     var activeimg = (document.querySelectorAll('#photos img')[0].img-index);
     document.getElementById('photos').innerHTML = '<img id="jpg' + parseInt(activeimg - 1) + '" data-index="' + parseInt(activeimg - 1) + '" src="' + img[parseInt(activeimg-1)] + '"  class="picsize"/>'
-} 
+} */
