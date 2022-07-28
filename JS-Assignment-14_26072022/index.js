2aada6a6e897c38010663a4035f80eaa22640158
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-07-28 23:17:49 +0530

    slider

--- JS-Assignment-14_26072022/index.js
+++ JS-Assignment-14_26072022/index.js
@@ -3,17 +3,17 @@ var img = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg']
 
 document.getElementById('photos').innerHTML = '<img id="jpg0" data-image="0"  src="' + img[0] + '" class="picsize"/>'
 
- var dots = '';
+ var count = '';
 for (var i = 0 ; i < img.length; i++) {
-    dots += '<span onclick=" showslide('+i+')" class=" divslide" > '+i+' </span>'
+    count += '<span onclick=" showslide('+i+')" class=" tabs" > '+i+' </span>'
 }
-document.getElementById('dots').innerHTML=dots;
+document.getElementById('count').innerHTML=count;
 
-function showslide(0){
-    document.getElementById('photos').innerHTML = '<img id="img' +(0)+'"  data-image="' +(0)+ '" src="' + img[(0)] + '" />'
+function showslide(show){
+    document.getElementById('photos').innerHTML = '<img id="img' +(show)+'"  data-image="' +(show)+ '" src="' + img[(show)] + '" class="picsize"/>'
 }
 
-function ahead() {
+/*function ahead() {
     var activeimg = parseInt(document.querySelectorAll('#photos img ')[0].dataset.image);
     document.getElementById('photos').innerHTML = '<img id="jpg' +(activeimg + 1)+'"  data-image="' +(activeimg + 1)+ '" src="' + img[(activeimg + 1)] + '"  class="picsize"/>'
 
@@ -23,14 +23,4 @@ function back() {
     document.getElementById('photos').innerHTML = '<img id="jpg' +(activeimg - 1)+ '"  data-image="' +(activeimg - 1)+ '" src="' + img[(activeimg - 1)] + '" class="picsize"/>'
 
 }
-
-/*function forward() {
-    var activeimg = (document.querySelectorAll('#photos img')[0].img-index);
-    document.getElementById('photos').innerHTML = '<img id="jpg' + parseInt(activeimg + 1) + '" data-index="' + parseInt(activeimg + 1) + '" src="' + img[parseInt(activeimg+1)] + '"  class="picsize"/>'
-
-} 
-
-function backward() {
-    var activeimg = (document.querySelectorAll('#photos img')[0].img-index);
-    document.getElementById('photos').innerHTML = '<img id="jpg' + parseInt(activeimg - 1) + '" data-index="' + parseInt(activeimg - 1) + '" src="' + img[parseInt(activeimg-1)] + '"  class="picsize"/>'
-} */
+ */
