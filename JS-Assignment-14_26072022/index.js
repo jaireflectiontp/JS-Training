856b92482b552b31cd1ac65c00f4c53dd446ec11
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-07-29 12:20:48 +0530

    slider

--- JS-Assignment-14_26072022/index.js
+++ JS-Assignment-14_26072022/index.js
@@ -4,16 +4,16 @@ var img = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg']
 document.getElementById('photos').innerHTML = '<img id="jpg0" data-image="0"  src="' + img[0] + '" class="picsize"/>'
 
  var count = '';
-for (var i = 0 ; i < img.length; i++) {
-    count += '<span onclick=" showslide('+i+')" class=" tabs" > '+i+' </span>'
+for (var i = 1 ; i <= img.length; i++) {
+    count += '<span onclick=" slide('+(i-1)+')" class=" tabs" > '+i+' </span>'
 }
 document.getElementById('count').innerHTML=count;
 
-function showslide(show){
+function slide(show){
     document.getElementById('photos').innerHTML = '<img id="img' +(show)+'"  data-image="' +(show)+ '" src="' + img[(show)] + '" class="picsize"/>'
 }
 
-/*function ahead() {
+function ahead() {
     var activeimg = parseInt(document.querySelectorAll('#photos img ')[0].dataset.image);
     document.getElementById('photos').innerHTML = '<img id="jpg' +(activeimg + 1)+'"  data-image="' +(activeimg + 1)+ '" src="' + img[(activeimg + 1)] + '"  class="picsize"/>'
 
@@ -23,4 +23,4 @@ function back() {
     document.getElementById('photos').innerHTML = '<img id="jpg' +(activeimg - 1)+ '"  data-image="' +(activeimg - 1)+ '" src="' + img[(activeimg - 1)] + '" class="picsize"/>'
 
 }
- */
+ 
