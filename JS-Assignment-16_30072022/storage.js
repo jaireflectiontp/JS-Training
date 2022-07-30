e75fe1c69a38d44d0d606782bda93dcb0d0f835b
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-07-31 00:12:34 +0530

    local storage

---
+++ JS-Assignment-16_30072022/storage.js
@@ -0,0 +1,3 @@
+document.getElementById('firstname').innerHTML=localStorage.getItem('firstname')
+document.getElementById('lastname').innerHTML=localStorage.getItem('lastname')
+document.getElementById('mail').innerHTML=localStorage.getItem('mail')
