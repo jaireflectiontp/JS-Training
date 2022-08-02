a8573bbfecb6bb34f20d5dc35e91a0e35a1bead6
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-08-03 00:09:34 +0530

    jquery

--- JS-Assignment-17_02082022/jquery.js
+++ JS-Assignment-17_02082022/jquery.js
@@ -1,20 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.bronco, .mustang, .f150').click(function () {
 
-        $('.img1, .img2, .img-3').hide()
-        
-        if ($(this).hasClass('bronco')) {
+        //$('.img1, .img2, .img-3').hide()
 
-            $('.img1').slideToggle();
+        if ($(this).hasClass('bronco')) {
+            $('.img2').removeClass('active')
+            $('.img3').removeClass('active')
+            if ($('.img1').hasClass('active')) {
+               $('.img1').removeClass('active')
+            }
+            else {
+                $('.img1').fadeToggle();
+            }
+            
             $(this).toggleClass('active')
         }
         else if ($(this).hasClass('mustang')) {
-            $('.img2').fadeToggle();
+            $('.img1').removeClass('active')
+            $('.img3').removeClass('active')
+            if ($('.img2').hasClass('active')) {
+                $('.img2').removeClass('active')
+            }
+            else {
+                $('.img2').fadeToggle();
+            }
+            
             $(this).toggleClass('active')
         }
         else {
-            $('.img3').slideToggle();
+            $('.img2').removeClass('active')
+            $('.img1').removeClass('active')
+            if ($('.img3').hasClass('active')) {
+                $('.img3').removeClass('active')
+            }
+            else {
+                $('.img3').fadeToggle();
+            }
+            
             $(this).toggleClass('active')
         }
     })
