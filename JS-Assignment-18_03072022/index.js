ae32169cba3776750b512e6e36a6a74905e939ef
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-08-04 12:54:19 +0530

    storage animate in jquery

--- JS-Assignment-18_03072022/index.js
+++ JS-Assignment-18_03072022/index.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
-  $('input').keyup(function(){
-    var store = $(this).val();
-    $('#store').html(store);
-    localStorage.setItem('store', store);
-    $('store').html=localStorage.getItem('store')
-    $('store').addClass(active);
-    if($('store').hasClass('active')){
-    $('store').removeClass('active')
-    }
-    else{
-        ('store').addClass('active')
-    }
+$(document).ready(function () {
+
+
+  $('input').keyup(function () {
+      var store = $(this).val();
+      $('#store').html(store);
+
+      localStorage.setItem('store', store);
+      $('store').html = localStorage.getItem('store')
+
+    })
+  $('input').click(function () {
+    $(this).animate({ 'width': '250px' }, 600)
+    $(this).css({ 'box-shadow': '0 0 7px 2px grey' })
+    $('.store').css({'display':'block'})
   })
-    $('input').click(function(){
-        $(this).animate({'width':'250px'},600,)
-    })
-    $('input').blur(function(){
-        $(this).animate({'width':'162.4px'},0)
-    })
-   
+  $('input').blur(function () {
+    $(this).animate({ 'width': '162.4px' }, 0)
+    $(this).css({ 'box-shadow': 'none' }, { 'outline': 'none' })
+    $('.store').css({'display':'none'})
+  })
+
 })
