fd0afa8012339c72208f344727332392cb8936f2
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-08-08 20:41:51 +0530

    calc using class

--- JS-Assignment-23_07082022/calc.js
+++ JS-Assignment-23_07082022/calc.js
@@ -1,20 +1,23 @@
-
 var result = '';
-var numb1 = document.getElementById('num1').value;
-console.log(numb1)
-var numb2 = document.getElementById('num2').value;
-console.log(numb1)
+
 class calculator {
 constructor(number1, number2, operator) {
     this.number1 = number1;
     this.number2 = number2;
     this.operator = operator;
 }
+getnumber1(){
+    return parseInt(document.getElementById('num1').value);
+}
+getnumber2(){
+    return parseInt(document.getElementById('num2').value);    
+}
 calculate(){
+    
     if (this.operator == '+') {
         result = (this.number1 + this.number2)
     }
-    else if (this.operator == '-') {
+    else if (this.operator == '-') { 
          result = this.number1 - this.number2
      }
      else if (this.operator == '*') {
@@ -26,8 +29,32 @@ calculate(){
      document.getElementById('result').innerHTML = result;
 }
 }
+function addnew(){
+    let getnum=new calculator()
+    var numb1 = getnum.getnumber1(); 
+    var numb2 = getnum.getnumber2();
+    let add = new calculator(numb1,numb2, '+');
+    add.calculate()
+}
+function substractnew(){
+    let getnum=new calculator()
+    var numb1 = getnum.getnumber1(); 
+    var numb2 = getnum.getnumber2();
+    let substract = new calculator(numb1,numb2, '-');
+    substract.calculate()
+}
+function multiplynew(){
+    let getnum=new calculator()
+    var numb1 = getnum.getnumber1(); 
+    var numb2 = getnum.getnumber2();
+    let multiply = new calculator(numb1,numb2, '*');
+    multiply.calculate()
+}
+function dividenew(){
+    let getnum=new calculator()
+    var numb1 = getnum.getnumber1(); 
+    var numb2 = getnum.getnumber2();
+    let divide = new calculator(numb1,numb2, '/');
+    divide.calculate()
+}
 
-let add = new calculator (numb1, numb2, '+');
-let substract = new calculator(numb1,numb2, '-');
-let multiply = new calculator(numb1, numb2, '*');
-let divide = new calculator(numb1, numb2, '/');
