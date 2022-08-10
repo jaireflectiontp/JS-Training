9e5e3e8ffe727934861f77150586ec76b5f1813a
jai.reflectiontp <jai.reflectiontp@gmail.com>
2022-08-10 21:43:11 +0530

    syntax

---
+++ Syntax/syntax.js
@@ -0,0 +1,31 @@
+//...........................#1 Class.......................................................
+class classname {
+    constructor(){
+
+    }
+    Nfunction(){
+
+    }
+}
+let variable = new classname()
+variable.Nfunction()
+
+//...........................#2 Extends.....................................................
+class mainclass {
+    constructor(){
+
+    }
+    Nfunction(){
+
+    }
+}
+let var1 = new classname()
+class afterclass {
+    constructor(){
+        super();
+    }
+    function(){
+
+    }
+}
+let var2 = new afterclass()
