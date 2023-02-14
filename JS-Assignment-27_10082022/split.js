1591ba990d2758bbecaebb71ed908fa8c6d16589
jaireflectiontp <jai.reflectiontp@gmail.com>
2023-02-13 22:05:41 -0800

    Assignment 27 update

--- JS-Assignment-27_10082022/split.js
+++ JS-Assignment-27_10082022/split.js
@@ -1,18 +1,24 @@
 //Split Method
-exp='one, two, three, four';
+/*exp='one, two, three, four';
 var arr=exp.split(',');
-console.log(arr)
+console.log(arr)*/
 //Replace Method
 //Example-1
-examp="HTML is the standard markup language for Online pages HTML is the standard markup language for Online pagesHTML is the standard markup language for Online pages";
-var show = examp.replaceAll('Online','Web');
+const take=(event)=>{
+    var word=document.getElementById('inpt').value;
+    console.log(word)
+}
+
+examp=document.getElementById('para').value;
+var show = examp.replace(p,word);
 console.log(show)
+document.getElementById('para').value=show
 
 //For
-add=['one, two, five, four'];
+/*add=['one, two, five, four'];
 for(var i of add){
     if(i=='five'){
         console.log(i)
     }
 
-}
+}*/
