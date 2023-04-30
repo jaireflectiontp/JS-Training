a8a40b6ccde2d15b56884b2bea9b26208144c3b9
jaireflectiontp <jai.reflectiontp@gmail.com>
2023-04-30 14:07:03 +0530

    Switch statement accordion

---
+++ JS-Assignment-8_21072022(Accordion_using_switch_statement)/nav.js
@@ -0,0 +1,33 @@
+function show(id) {
+
+    switch (id) {
+
+        case 'why':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+                document.getElementsByClassName('btn_icon').style.display = 'block'
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+
+        case 'p&s':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+
+        case 'learn':
+            if (document.getElementById(id).style.display == 'none') {
+                document.getElementById(id).style.display = 'block';
+            }
+            else {
+                document.getElementById(id).style.display = 'none';
+            }
+            break;
+    }
+}
